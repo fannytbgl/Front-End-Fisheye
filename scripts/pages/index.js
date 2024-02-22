103ae750eadc97568251810eeeb3e932c23a82fc
ftbgl <tabaglio.fanny@gmail.com>
2024-02-22 16:26:21 +0100

    array of json datas is fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,21 @@
+
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        try {
+            const photographersDataResponse = await fetch('./data/photographers.json');
+    
+            if (!photographersDataResponse.ok) {
+                throw new Error('Erreur de chargement des données des photographes');
+            }
+    
+            const photographersData = await photographersDataResponse.json();
+    
+            console.log(photographersData);
+    
+            return photographersData;
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données ', error);
+            // Gérez l'erreur selon vos besoins
+        }
     }
 
     async function displayData(photographers) {
@@ -37,9 +29,14 @@
     }
 
     async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        try {
+            // Récupère les datas des photographes
+            const photographers = await getPhotographers();
+            console.log(photographers);
+        } catch (error) {
+            console.error('Erreur lors de l\'initialisation ', error);
+            // Gérez l'erreur selon vos besoins
+        }
     }
     
     init();
